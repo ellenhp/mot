878e48798cf5229b29d2a373edd5a44b1e8c500b
Ellen Poe <ellen.h.poe@gmail.com>
2025-08-12 14:53:27 -0700

    schema stuff

--- osm/osm2pgsql/post_run.sql
+++ osm/osm2pgsql/post_run.sql
@@ -7,6 +7,7 @@ CREATE TABLE poi (
   geom GEOMETRY
 );
 
+-- poi.geom may be any type of geometry except `*LineString` so we can't specify the SRID in the column definition. Change it here.
 SELECT UpdateGeometrySRID('poi', 'geom', 4326);
 
 INSERT INTO poi (way_id, tags, geom) SELECT way_id, tags, geom FROM way_poi;
@@ -50,4 +51,58 @@ SELECT
 FROM tile_coords;
 
 CREATE INDEX IF NOT EXISTS tiles_geom ON tiles USING GIST(geom);
+
+CREATE OR REPLACE VIEW edge_intersections
+AS (SELECT DISTINCT
+    (ST_Dump(ST_Intersection(l1.geom,l2.geom))).geom AS geom,
+    l1.way_id AS way_id,
+    l2.way_id AS transition_to_way
+    FROM lines AS l1
+    INNER JOIN lines AS l2 ON ST_Intersects(l2.geom, l1.geom)
+    WHERE l1.way_id <> l2.way_id
+);
+
+CREATE OR REPLACE VIEW edge_transitions
+AS (SELECT DISTINCT
+    ST_Transform(intersections.geom, 4326) AS intersection_geom,
+    ST_Length(ST_LineSubstring(lines.geom, 0.0, ST_LineLocatePoint(lines.geom, intersections.geom))::geography) AS distance_along_way,
+    ST_Length(ST_LineSubstring(lines2.geom, 0.0, ST_LineLocatePoint(lines2.geom, intersections.geom))::geography) AS transition_to_distance_along_way,
+    intersections.way_id AS way_id,
+    intersections.transition_to_way AS transition_to_way,
+    lines.tags AS way_tags,
+    lines2.tags AS transition_to_way_tags,
+    restrictions.tags AS restriction_tags
+    FROM lines
+    LEFT JOIN edge_intersections AS intersections ON lines.way_id = intersections.way_id
+    LEFT JOIN lines as lines2 on intersections.transition_to_way = lines2.way_id
+    LEFT JOIN restrictions ON restrictions.from = intersections.way_id AND restrictions.to = intersections.transition_to_way
+    WHERE ST_Length(intersections.geom) = 0 -- Fixes: error returned from database: Splitter line has linear intersection with input
+);
+
+-- Create the intersections table
+DROP TABLE IF EXISTS intersections;
+CREATE TABLE intersections (
+    way_id BIGINT,
+    transition_to_way BIGINT,
+    distance_along_way REAL,
+    restriction_tags JSONB,
+    geom GEOMETRY(Point, 4326)
+);
+
+-- Populate the intersections table
+INSERT INTO intersections (
+    way_id,
+    transition_to_way,
+    distance_along_way,
+    restriction_tags,
+    geom
+)
+SELECT
+    way_id,
+    transition_to_way,
+    restriction_tags,
+    intersection_geom
+FROM edge_transitions;
+
+CREATE INDEX IF NOT EXISTS idx_intersections_geom ON intersections USING GIST(geom);
 
