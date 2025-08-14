a220f622eb4484b3bfeb2d0d733cd7ca7f8a21d7
Ellen Poe <ellen.h.poe@gmail.com>
2025-08-13 18:53:23 -0700

    refactor: restructure directories

--- data/osm2pgsql/post_run.sql
+++ data/osm2pgsql/post_run.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS poi;
-
 CREATE TABLE poi (
+  id SERIAL,
   way_id BIGINT,
   node_id BIGINT,
   tags JSONB,
@@ -10,7 +10,8 @@ CREATE TABLE poi (
 -- poi.geom may be any type of geometry except `*LineString` so we can't specify the SRID in the column definition. Change it here.
 SELECT UpdateGeometrySRID('poi', 'geom', 4326);
 
-INSERT INTO poi (way_id, tags, geom) SELECT way_id, tags, geom FROM way_poi;
+INSERT INTO poi (way_id, tags, geom) SELECT way_id, tags, ST_Centroid(geom) FROM way_poi WHERE ST_Area(geom::GEOGRAPHY, false) < 2000;
+INSERT INTO poi (way_id, tags, geom) SELECT way_id, tags, geom FROM way_poi WHERE ST_Area(geom::GEOGRAPHY, false) >= 2000;
 INSERT INTO poi (node_id, tags, geom) SELECT node_id, tags, geom FROM node_poi;
 
 CREATE INDEX poi_geom_idx ON poi USING GIST(geom);
@@ -57,24 +58,24 @@ AS (SELECT DISTINCT
     (ST_Dump(ST_Intersection(l1.geom,l2.geom))).geom AS geom,
     l1.way_id AS way_id,
     l2.way_id AS transition_to_way
-    FROM lines AS l1
-    INNER JOIN lines AS l2 ON ST_Intersects(l2.geom, l1.geom)
+    FROM roads AS l1
+    INNER JOIN roads AS l2 ON ST_Intersects(l2.geom, l1.geom)
     WHERE l1.way_id <> l2.way_id
 );
 
 CREATE OR REPLACE VIEW edge_transitions
 AS (SELECT DISTINCT
     ST_Transform(intersections.geom, 4326) AS intersection_geom,
-    ST_Length(ST_LineSubstring(lines.geom, 0.0, ST_LineLocatePoint(lines.geom, intersections.geom))::geography) AS distance_along_way,
-    ST_Length(ST_LineSubstring(lines2.geom, 0.0, ST_LineLocatePoint(lines2.geom, intersections.geom))::geography) AS transition_to_distance_along_way,
+    ST_Length(ST_LineSubstring(roads.geom, 0.0, ST_LineLocatePoint(roads.geom, intersections.geom))::geography) AS distance_along_way,
+    ST_Length(ST_LineSubstring(roads2.geom, 0.0, ST_LineLocatePoint(roads2.geom, intersections.geom))::geography) AS transition_to_distance_along_way,
     intersections.way_id AS way_id,
     intersections.transition_to_way AS transition_to_way,
-    lines.tags AS way_tags,
-    lines2.tags AS transition_to_way_tags,
+    roads.tags AS way_tags,
+    roads2.tags AS transition_to_way_tags,
     restrictions.tags AS restriction_tags
-    FROM lines
-    LEFT JOIN edge_intersections AS intersections ON lines.way_id = intersections.way_id
-    LEFT JOIN lines as lines2 on intersections.transition_to_way = lines2.way_id
+    FROM roads
+    LEFT JOIN edge_intersections AS intersections ON roads.way_id = intersections.way_id
+    LEFT JOIN roads as roads2 on intersections.transition_to_way = roads2.way_id
     LEFT JOIN restrictions ON restrictions.from = intersections.way_id AND restrictions.to = intersections.transition_to_way
     WHERE ST_Length(intersections.geom) = 0 -- Fixes: error returned from database: Splitter line has linear intersection with input
 );
@@ -100,6 +101,7 @@ INSERT INTO intersections (
 SELECT
     way_id,
     transition_to_way,
+    distance_along_way,
     restriction_tags,
     intersection_geom
 FROM edge_transitions;
